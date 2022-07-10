a637d392f5329e496fa76d10e115c0866bbde4de
Chuy <jesus.morales@rabbitmx.com>
2022-07-09 22:31:43 -0500

    feat: first version to deploy to heroku

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3001);
+  await app.listen(PORT || 3001);
 }
 bootstrap();
