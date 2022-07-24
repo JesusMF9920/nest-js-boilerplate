df007024fe358187070fc5a65bbc8c55d1f5c752
Chuy <jesus.morales@rabbitmx.com>
2022-07-24 00:26:06 -0500

    feat: register account test

--- test/v1/accounts/register-driver.spec.ts
+++ test/v1/accounts/register-driver.spec.ts
@@ -10,4 +10,21 @@ describe('POST /api/v1/auth/accounts/registration', () => {
     expect(body).toHaveProperty('accessToken');
     expect(body).toHaveProperty('accountId');
   });
+
+  it('throws an error when registering with an preexisting id', async () => {
+    const client = await createClient();
+
+    await client.registerUser({ id: MARIA.id }).expect(201).run();
+    await client.registerUser({ id: MARIA.id, email: 'jesusfndz437@gmail.com' }).expect(409).run();
+  });
+  it('throws an error if there are two registrations with the same email', async () => {
+    const client = await createClient();
+
+    const AN_EMAIL = 'jordimoreno@gmail.com';
+    const SAME_EMAIL_CAPITALIZED = 'Jordimoreno@gmail.com';
+
+    await client.registerUser({ email: AN_EMAIL }).expect(201).run();
+
+    await client.registerUser({ email: SAME_EMAIL_CAPITALIZED }).expect(409).run();
+  });
 });
